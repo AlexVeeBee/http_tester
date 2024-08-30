4b3b97ce977924f72dccef9a848729a9ffd592e6
Alex <alex200.veebee@gmail.com>
2024-08-30 15:31:25 +0000

    Ver 0.2.1 auto updater test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import HttpTester from "./pages/httpTester";
+import { check } from "@tauri-apps/plugin-updater"
+
+
+const isUpdateAvailable = async () => {
+  console.log("Checking for updates");
+  try {
+    const update = await check();
+    console.log("Update: ", update);
+    return update;
+  } catch (e) {
+    console.error("Failed to check for updates: ", e);
+    return false;
+  }
+}
 
 function App() {
+  const [updateAvailable, setUpdateAvailable] = useState(false);
+  const [updateError, setUpdateError] = useState(false);
+  const onUpdate = () => {
+    setUpdateAvailable(true);
+  };
+
+  useEffect(() => {
+    (async () => {
+      const available = await isUpdateAvailable()
+      if (available) {
+        onUpdate();
+      } else {
+        setUpdateAvailable(false);
+        setUpdateError(true);
+      }
+    })()
+  }, []);
+
   return (
     <>
+      {updateAvailable && <div>Update available</div>}
+      {updateError && <div>Failed to check for updates</div>}
       <HttpTester />
     </>
   );
